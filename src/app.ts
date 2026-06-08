ba61cf59f75e4126f228a6b1dee8d434a3eacc77
easternferdinan <ferdinandrestu03@gmail.com>
2026-06-08 16:30:41 +0700

    feat(api): add response logger

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
+
 import authRouter from "./routes/auth.routes.js";
+import { responseLogger } from "./middlewares/logger.middleware.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(responseLogger);
 
 app.use("/api/auth", authRouter);
 export default app;
---
+++ src/middlewares/logger.middleware.ts
@@ -0,0 +1,11 @@
+import type { RequestHandler } from "express";
+
+export const responseLogger: RequestHandler = (req, res, next) => {
+    res.on("finish", () => {
+        console.log(
+            `[${res.statusCode} - ${res.statusMessage}] ${req.method}: ${req.originalUrl}`
+        );
+    });
+
+    next();
+};
