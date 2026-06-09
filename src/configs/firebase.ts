f1d2a8ef27fb8ecc408b263fb4dec9d8a2b54115
easternferdinan <ferdinandrestu03@gmail.com>
2026-06-09 11:54:22 +0700

    !feat: migrate firebase client sdk to firebase admin sdk

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -1,24 +1,13 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { env } from "./env.js";
+import { initializeApp, cert } from "firebase-admin/app";
+import type { ServiceAccount } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
+import serviceAccount from "./firebaseAdminSdkKey.json" with { type: "json" };
 
-// https://firebase.google.com/docs/web/setup#available-libraries
+const app = initializeApp({
+  credential: cert(serviceAccount as ServiceAccount),
+});
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: env.FIREBASE_API_KEY,
-  authDomain: env.FIREBASE_AUTH_DOMAIN,
-  projectId: env.FIREBASE_PROJECT_ID,
-  storageBucket: env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: env.FIREBASE_APP_ID,
-  measurementId: env.FIREBASE_MEASUREMENT_ID,
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const firestore = getFirestore(app);
+
